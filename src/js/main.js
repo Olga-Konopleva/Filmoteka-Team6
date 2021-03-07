171201dae90d486871a96cde7f3cce970f0824dd
Olga Konopleva <olenenok.she@gmail.com>
2021-03-07 18:39:33 +0200

    исправлена опечатка в названии функции updateGenres

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 import footerModalHandler from './FooterModalHandler/FooterModalHandler';
 import apiService from '../apiServises/api';
-import { updateGalleryMarkup } from './update-markup';
+import updateGalleryMarkup from './update-markup';
 
 footerModalHandler('#students');
 
 // удалить(уточнить у Оли)
-// apiService
-//   .getUpdatedFilms(apiService.getUrl().popylarFilmsUrl)
-//   .then(updateGalleryMarkup);
+apiService
+  .getUpdatedFilms(apiService.getUrl().popylarFilmsUrl)
+  .then(updateGalleryMarkup);
 
