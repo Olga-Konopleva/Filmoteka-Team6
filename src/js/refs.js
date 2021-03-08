468d601261b07d11545a68c4d370caa344b6ae4a
Olga Konopleva <olenenok.she@gmail.com>
2021-03-08 19:47:30 +0200

    добавлен скрипт открытия списка поиска

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,7 +2,8 @@ const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('.js-gallery'),
   galleryContainer: document.querySelector('.gallery__list'),
-
+  searchList: document.querySelector('.js-search-list'),
+  input: document.querySelector('.js-input'),
   libraryBtn: document.querySelector('#libraryBtn'),
   homeBtn: document.querySelector('#homeBtn'),
   header: document.querySelector('#header'),
--- src/js/search-films.js
+++ src/js/search-films.js
@@ -5,6 +5,7 @@ refs.searchForm.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
+  refs.searchList.classList.add('is-hidden');
   const form = event.currentTarget;
   api.query = form.elements.query.value;
   refs.gallery.textContent = '';
