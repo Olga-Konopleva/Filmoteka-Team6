0997f84d490cb82a50c7b6d4fb31df337e302110
AlexandraMarkova <a.markova@ukr.net>
2021-03-09 13:40:25 +0200

    открывает модальное окно по нажатию на li

--- src/js/modal-single-film.js
+++ src/js/modal-single-film.js
@@ -1,50 +1,47 @@
-// пока еще сырой вариант по конкретному id
 
 import MicroModal from 'micromodal';
-import axios from 'axios';
+// import axios from 'axios';
 import filmTpl from '../templates/modal.hbs';
 import refs from '../js/refs';
+import  apiService  from '../apiServises/api';
 
 
-// const buttonModal = document.querySelector('.btn-trigger');
-// const divModal = document.querySelector('.modal__container');
+refs.gallery.addEventListener('click', openModal);
 
-// MicroModal.init({
-//   onShow: modal => console.info(`${modal.id} is shown`), // [1]
-//   onClose: modal => console.info(`${modal.id} is hidden`), // [2]
-//   openTrigger: 'data-custom-open', // [3]
-//   closeTrigger: 'data-custom-close', // [4]
-//   openClass: 'is-open', // [5]
-//   disableScroll: true, // [6]
-//   disableFocus: false, // [7]
-//   awaitOpenAnimation: false, // [8]
-//   awaitCloseAnimation: false, // [9]
-//   debugMode: true // [10]
-// });
-
-refs.buttonModal.addEventListener('click', () => {
+export function openModal() { 
+const cards = refs.gallery.querySelectorAll('li');
+cards.forEach(card => card.addEventListener('click', showModal));
   MicroModal.init({
-    onClose: modal => console.info(`${'modal-1'} is hidden`),
-    onShow: modal => console.info(`${'modal-1'} is shown`),
-    disableScroll: true,
+  //  onClose: modal => console.info(`${'modal-1'} is hidden`),
+  //  onShow: modal => console.info(`${'modal-1'} is shown`),
+   disableScroll: true,
     disableFocus: true,
   });
-});
+}
 
-
-axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-const apiKey = 'e3cac6b09ca16e2df27fc1a61005a6af';
-
-const showFilmDetails = async id => {
-  const { data } = await axios.get(
-    `/movie/${id}?api_key=${apiKey}&language=en-US`,
-  );
-  console.log(data);
-  updateData(data);
-};
-showFilmDetails(587806);
+function showModal(event) {
+  const element = event.currentTarget;
+  const id = element.dataset.id;
+  console.log(id);
+  
+  apiService.showFilmDetails(id).then(updateData).then(openModal);
+  refs.divModal.innerHTML = '';
+}
 
 function updateData(data) {
   const markup = filmTpl(data);
   refs.divModal.insertAdjacentHTML('beforeend', markup);
 }
+
+// axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+// const apiKey = 'e3cac6b09ca16e2df27fc1a61005a6af';
+
+// const showFilmDetails = async id => {
+//   const { data } = await axios.get(
+//     `/movie/${id}?api_key=${apiKey}&language=en-US`,
+//   );
+//   console.log(data);
+//   updateData(data);
+// };
+
+// showFilmDetails(587806);
