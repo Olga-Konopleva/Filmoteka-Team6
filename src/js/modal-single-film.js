acd42940fb97e75430122e501ef695744594fb16
AlexandraMarkova <a.markova@ukr.net>
2021-03-10 21:44:23 +0200

    меняет размеры постера, фиксит открытие модалки,  фиксит стили, убирает скролл за модалкой, добавляет скролл для модалки на моб версию

--- src/js/modal-single-film.js
+++ src/js/modal-single-film.js
@@ -5,33 +5,21 @@ import apiService from '../apiServises/api';
 
 refs.gallery.addEventListener('click', openModal);
 
-MicroModal.init({
-  onShow: modal => console.info(`${modal.id} is shown`), // [1]
-  onClose: modal => console.info(`${modal.id} is hidden`), // [2]
-  openTrigger: 'data-custom-open', // [3]
-  closeTrigger: 'data-custom-close', // [4]
-  openClass: 'is-open', // [5]
-  disableScroll: true, // [6]
-  disableFocus: true, // [7]
-  awaitOpenAnimation: false, // [8]
-  awaitCloseAnimation: false, // [9]
-  debugMode: true, // [10]
-});
-
 export function openModal(event) {
-  // console.log(event.target.nodeName);
   if (
     event.target.nodeName === 'UL'
-    // event.target.nodeName !== 'IMG' &&
-    // event.target.nodeName !== 'LI' &&
-    // event.target.nodeName !== 'H3' &&
-    // event.target.nodeName !== 'P'
   ) {
     return;
   }
   showModal(event);
-  MicroModal.show('modal-1');
-}
+  
+  MicroModal.show('modal-1', {
+    onShow: modal => console.info(`${modal.id} is shown`), // [1]
+    onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+    disableScroll: true, // [6]
+    disableFocus: true, // [7]
+  })
+};
 
 function showModal(event) {
   const element = event.target;
@@ -41,13 +29,13 @@ function showModal(event) {
   apiService
     .showFilmDetails(id)
     .then(data => apiService.updateOneFilmInfo(data))
-    //  .then(console.log)
     .then(data => updateData(data));
   refs.divModal.innerHTML = '';
+ 
 }
 
-function updateData(data) {
-  const markup = filmTpl(data);
-  refs.divModal.insertAdjacentHTML('beforeend', markup);
-  return markup;
-}
+  function updateData(data) {
+    const markup = filmTpl(data);
+    refs.divModal.insertAdjacentHTML('beforeend', markup);
+    return markup;
+  }
