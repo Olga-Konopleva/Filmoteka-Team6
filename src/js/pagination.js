46f070aaa32e8336cdea275fad64a71a61aae073
yurii-shendryk <yuriishendryk93@gmail.com>
2021-03-12 18:02:47 +0200

    додав пагінацыю для пошуку фільмів

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -7,6 +7,7 @@ import { BASE_URL, API_KEY } from '../apiServises/api-variables';
 import refs from './refs';
 
 const startPage = 1;
+let screenSize = document.documentElement.clientWidth;
 const options = {
   totalItems: 1,
   itemsPerPage: 1,
@@ -17,26 +18,20 @@ const options = {
 };
 
 const addItems = array => {
-  if (
-    document.documentElement.clientWidth >= 320 &&
-    document.documentElement.clientWidth < 768
-  ) {
+  if (screenSize >= 320 && screenSize < 768) {
     return array.map(film => film).slice(0, 4);
   }
-  if (
-    document.documentElement.clientWidth >= 768 &&
-    document.documentElement.clientWidth < 1024
-  ) {
+  if (screenSize >= 768 && screenSize < 1024) {
     return array.map(film => film).slice(0, 8);
   }
-  if (document.documentElement.clientWidth >= 1024) {
+  if (screenSize >= 1024) {
     return array.map(film => film).slice(0, 9);
   }
 };
 
-const addPagination = () => {
-  const pagination = new Pagination('pagination', options);
-  pagination.on('beforeMove', async evt => {
+const addPaginationForPopularFilms = () => {
+  const paginationPopular = new Pagination('pagination', options);
+  paginationPopular.on('beforeMove', async evt => {
     const { page } = evt;
     const data = await api.getUpdatedFilms(api.getUrl(page).popylarFilmsUrl);
     const results = addItems(data);
@@ -57,14 +52,43 @@ const addPagination = () => {
   });
 };
 
-const start = async () => {
+const addPaginationForSearch = () => {
+  const paginationSearch = new Pagination('pagination', options);
+  paginationSearch.on('beforeMove', async evt => {
+    const { page } = evt;
+    const data = await api.getUpdatedFilms(api.getUrl(page).searchUrl);
+    const results = addItems(data);
+    console.log(results);
+    if (results) {
+      refs.gallery.innerHTML = '';
+
+      refs.spinner.classList.remove('hide');
+      window.scrollTo({
+        top: document.documentElement.offsetHeight,
+        behavior: 'smooth',
+      });
+      updateGalleryMarkup(results);
+      refs.spinner.classList.add('hide');
+    } else {
+      return false;
+    }
+  });
+};
+
+const startPopularPagination = async () => {
   const { data } = await axios.get(api.getUrl(startPage).popylarFilmsUrl);
   const { total_pages } = await data;
   options.totalItems = total_pages;
 
-  addPagination();
+  addPaginationForPopularFilms();
 };
 
-start();
+const startSearchPagination = async () => {
+  const { data } = await axios.get(api.getUrl(startPage).searchUrl);
+  const { total_pages } = await data;
+  options.totalItems = total_pages;
 
-export { startPage, addItems };
+  addPaginationForSearch();
+};
+
+export { startPage, addItems, startPopularPagination, startSearchPagination };
