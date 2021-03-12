81214039110cafe064d419be230e1ca2c1355cf1
Vityuk Yuriy <you.thunderstruck@gmail.com>
2021-03-12 02:32:16 +0200

    добавляет запоминание кнопок без логина
    - в файле обрабтки модального окна, добави:
    1. рефсы на кнопки
    2. свою фукнцию актуализации стилей кнопок (адд/ремов)
    в Васином файле, просто дописал внизу свою функцию актуализацию кнопок.
    
    п.с - никак иначе выловить кнопки не получалось. у Васи они ловятся, потому что у него идет асинхроный запрос, и за это время модалка прогружается. у меня, асинхроного кода нет, потому и нужно было передавать уже все с рефсами.
    
    получилось не очень однотипно у нас с ним, но работает. Хотя код и желательно рефакторить.

--- src/js/mainFirebase.js
+++ src/js/mainFirebase.js
@@ -1,82 +1,126 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-import refs from './refs.js'
-import * as auth from './authFirebase'
-import * as dataToFirebase from './dataToFirebase.js'
-import * as dataFromFirebase from './dataFromFirebase'
+import refs from './refs.js';
+import * as auth from './authFirebase';
+import * as dataToFirebase from './dataToFirebase.js';
+import * as dataFromFirebase from './dataFromFirebase';
+
+import {
+  addFilmHandlerQueue,
+  deleteFilmHandlerQueue,
+  getListFilmsQueue,
+} from './localStorageQueue';
+import {
+  addFilmHandlerWatched,
+  deleteFilmHandlerWatched,
+  getListFilmsWatched,
+} from './localStorageWatched';
 
 const container = refs.divModal;
 
+// container.addEventListener('click', modalClick)
+
 //ФУНКЦІЯ ДЛЯ ОБРОБКИ КЛІКІВ ПО КНОПКАХ В МОДАЛЦІ
 function modalMagic() {
+  const addBtnWatch = container.querySelector('button[data-id=addwatched]');
+  const removeBtnWatch = container.querySelector(
+    'button[data-id=removewatched]',
+  );
+  const addBtnQueue = container.querySelector('button[data-id=addqueue]');
+  const removeBtnQueue = container.querySelector('button[data-id=removequeue]');
+  // console.log(addBtnWatch)
 
-    
-        const addBtnWatch = container.querySelector('button[data-id=addwatched]');
-        const removeBtnWatch = container.querySelector('button[data-id=removewatched]');
-        const addBtnQueue = container.querySelector('button[data-id=addqueue]');
-        const removeBtnQueue = container.querySelector('button[data-id=removequeue]');
-  
-  container.addEventListener('click', (event) => {
-        
-        dataFromFirebase.setModalBtnWatchStyles(event,addBtnWatch,removeBtnWatch);
-        dataFromFirebase.setModalBtnQueueStyles(event,addBtnQueue, removeBtnQueue);
-      });
-  
-
-
+  container.addEventListener('click', event => {
+    dataFromFirebase.setModalBtnWatchStyles(event, addBtnWatch, removeBtnWatch);
+    dataFromFirebase.setModalBtnQueueStyles(event, addBtnQueue, removeBtnQueue);
+  });
 }
 // }
-
-
 
 //ФУНКЦІЯ ДЛЯ ПЕРЕВІРКИ ЧИ Є АЙДІШНИК В БД І ВІДПОВІДНО
 //ЯКІ КНОПКИ ПОКАЗУВАТИ В МОДАЛЦІ
 function checkMovieInStorage() {
-  let currentUser = firebase.auth().currentUser
+  let currentUser = firebase.auth().currentUser;
   if (!currentUser) {
     return;
   }
-  
-    let id = localStorage.getItem('firebase-id');
 
-  auth.readUserData(currentUser.uid)
-    .then((data) => (data.val())).then((data) => {
+  let id = localStorage.getItem('firebase-id');
+
+  auth
+    .readUserData(currentUser.uid)
+    .then(data => data.val())
+    .then(data => {
       if (data.watched || data.queue) {
-          console.log("we are in!");
-        const dataFromWatched = data.watched ||[];
-        const dataFromQueue = data.queue ||[];
-          console.log("ok");
-          
-        const addBtnWatch = container.querySelector('button[data-id=addwatched]');
-        const removeBtnWatch = container.querySelector('button[data-id=removewatched]');
+        console.log('we are in!');
+        const dataFromWatched = data.watched || [];
+        const dataFromQueue = data.queue || [];
+        console.log('ok');
+
+        const addBtnWatch = container.querySelector(
+          'button[data-id=addwatched]',
+        );
+        const removeBtnWatch = container.querySelector(
+          'button[data-id=removewatched]',
+        );
         const addBtnQueue = container.querySelector('button[data-id=addqueue]');
-        const removeBtnQueue = container.querySelector('button[data-id=removequeue]');
+        const removeBtnQueue = container.querySelector(
+          'button[data-id=removequeue]',
+        );
 
         if (dataFromWatched.includes(id)) {
-          if (!addBtnWatch.classList.contains("hide")) {
-            
-              addBtnWatch.classList.add('hide');
-              removeBtnWatch.classList.remove('hide');
-          }
-          } else {
-            addBtnWatch.classList.remove('hide');
-              removeBtnWatch.classList.add('hide');
-        }
-        if (dataFromQueue.includes(id)) {
-          if (!addBtnQueue.classList.contains("hide")) {
-            
-              addBtnQueue.classList.add('hide');
-              removeBtnQueue.classList.remove('hide');
-
+          if (!addBtnWatch.classList.contains('hide')) {
+            addBtnWatch.classList.add('hide');
+            removeBtnWatch.classList.remove('hide');
+            // addToWatched.textContent = "ALREADY IN WATCHED";
+            // addToWatched.setAttribute("disabled", 'true');
           }
         } else {
-            addBtnQueue.classList.remove('hide');
-            removeBtnQueue.classList.add('hide');
+          addBtnWatch.classList.remove('hide');
+          removeBtnWatch.classList.add('hide');
+        }
+        if (dataFromQueue.includes(id)) {
+          if (!addBtnQueue.classList.contains('hide')) {
+            addBtnQueue.classList.add('hide');
+            removeBtnQueue.classList.remove('hide');
+            // addToQueue.textContent = "ALREADY IN QUEUE";
+            // addToQueue.setAttribute("disabled", 'true');
+          }
+        } else {
+          addBtnQueue.classList.remove('hide');
+          removeBtnQueue.classList.add('hide');
         }
       }
-    })
-
-  
+    });
 }
-export {checkMovieInStorage,modalMagic}
+function checkMovieInLocalStorage(
+  addBtnWatch,
+  removeBtnWatch,
+  addBtnQueue,
+  removeBtnQueue,
+) {
+  let id = localStorage.getItem('firebase-id');
+  const dataFromWatched = getListFilmsWatched() || [];
+  const dataFromQueue = getListFilmsQueue() || [];
+
+  if (dataFromWatched.includes(id)) {
+    if (!addBtnWatch.classList.contains('hide')) {
+      addBtnWatch.classList.add('hide');
+      removeBtnWatch.classList.remove('hide');
+    }
+  } else {
+    addBtnWatch.classList.remove('hide');
+    removeBtnWatch.classList.add('hide');
+  }
+  if (dataFromQueue.includes(id)) {
+    if (!addBtnQueue.classList.contains('hide')) {
+      addBtnQueue.classList.add('hide');
+      removeBtnQueue.classList.remove('hide');
+    }
+  } else {
+    addBtnQueue.classList.remove('hide');
+    removeBtnQueue.classList.add('hide');
+  }
+}
+export { checkMovieInStorage, modalMagic, checkMovieInLocalStorage };
--- src/js/modal-single-film.js
+++ src/js/modal-single-film.js
@@ -5,30 +5,27 @@ import apiService from '../apiServises/api';
 import { showTrailer } from './show-trailer';
 import * as mainFirebase from './mainFirebase';
 
-
 refs.gallery.addEventListener('click', openModal);
 
 export function openModal(event) {
-  if (
-    event.target.nodeName === 'UL'
-  ) {
+  if (event.target.nodeName === 'UL') {
     return;
   }
   showModal(event);
-  
+
   MicroModal.show('modal-1', {
     onShow: modal => console.info(`${modal.id} is shown`), // [1]
     onClose: modal => console.info(`${modal.id} is hidden`), // [2]
     disableScroll: true, // [6]
     disableFocus: true, // [7]
-  })
-};
+  });
+}
 
 export function showModal(event) {
   const element = event.target;
   const id = element.dataset.id;
   localStorage.setItem('firebase-id', id);
-  mainFirebase.checkMovieInStorage(id); 
+  mainFirebase.checkMovieInStorage(id);
   // console.log(id);
   apiService
     .showFilmDetails(id)
@@ -36,20 +33,34 @@ export function showModal(event) {
     //  .then(console.log)
     .then(data => updateData(data))
     .then(data => {
-      mainFirebase.checkMovieInStorage()
-      return data
+      mainFirebase.checkMovieInStorage();
+      const container = refs.divModal;
+      const addBtnWatch = container.querySelector('button[data-id=addwatched]');
+      const removeBtnWatch = container.querySelector(
+        'button[data-id=removewatched]',
+      );
+      const addBtnQueue = container.querySelector('button[data-id=addqueue]');
+      const removeBtnQueue = container.querySelector(
+        'button[data-id=removequeue]',
+      );
+      mainFirebase.checkMovieInLocalStorage(
+        addBtnWatch,
+        removeBtnWatch,
+        addBtnQueue,
+        removeBtnQueue,
+      );
+      return data;
     })
     .then(data => {
       mainFirebase.modalMagic();
-      return data
+      return data;
     })
     .then(showTrailer);
   refs.divModal.innerHTML = '';
- 
 }
 
-  function updateData(data) {
-    const markup = filmTpl(data);
-    refs.divModal.insertAdjacentHTML('beforeend', markup);
-    return markup;
-  }
+function updateData(data) {
+  const markup = filmTpl(data);
+  refs.divModal.insertAdjacentHTML('beforeend', markup);
+  return markup;
+}
